57a3be03dc37f752788195f0a40f8e59e6399583
Doktor Ziel <doktor@zielony-backlog.pl>
2020-10-28 20:45:08 +0100

    Example of mandatory option

---
+++ src/main/java/pl/backlog/green/Test04.java
@@ -0,0 +1,29 @@
+package pl.backlog.green;
+
+import org.apache.commons.cli.*;
+
+public class Test04 {
+    public static void main(String[] args) {
+        Option option = new Option("a", "first option without value, mandatory");
+        option.setRequired(true);
+
+        Options options = new Options();
+        options.addOption(option);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter help = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            help.printHelp("Test04", options);
+            System.exit(1);
+        }
+
+        if (cmd.hasOption("a")) {
+            System.out.println("Podałeś opcję 'a'");
+        }
+    }
+}
