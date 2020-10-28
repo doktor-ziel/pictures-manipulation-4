0ecc6c8e4870da94bda846ac8b30008e4c35b076
Doktor Ziel <doktor@zielony-backlog.pl>
2020-10-28 20:17:05 +0100

    Example of usage 3-argument-constructor

---
+++ src/main/java/pl/backlog/green/Test02.java
@@ -0,0 +1,33 @@
+package pl.backlog.green;
+
+import org.apache.commons.cli.*;
+
+public class Test02 {
+    public static void main(String[] args) {
+        Options options = new Options();
+
+        options.addOption(new Option("a", "first option without value, not mandatory"));
+        options.addOption(new Option("b", false,"second option without value, not mandatory"));
+        options.addOption(new Option("c", false,"third option with value, not mandatory"));
+
+
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter help = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            help.printHelp("Test01", options);
+            System.exit(1);
+        }
+
+        if (cmd.hasOption("a")) {
+            System.out.println("Podałeś opcję 'a'");
+        } else {
+            System.out.println("Nie podałeś opcji 'a'");
+        }
+    }
+}
